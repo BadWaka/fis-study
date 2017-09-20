fd8d896d0ecf92c01dcb53dd2038a3fcc0aa5af9
BadWaka <1456683844@qq.com>
2017-09-20 15:00:16 +0800

    基本学习完毕

--- demo-lv1/fis-conf.js
+++ demo-lv1/fis-conf.js
@@ -1,13 +1,50 @@
-fis.match('*', {
-  useHash: false
-});
+// // // fis.match('*', {
+// // //   useHash: false
+// // // });
 
-fis.match('::packager', {
+// // // fis.match('::packager', {
+// // //   postpackager: fis.plugin('loader', {
+// // //     //allInOne: true
+// // //   })
+// // // });
+
+// // // fis.match('*.{css,less}', {
+// // //   packTo: '/static/aio.css'
+// // // });
+
+// // fis.match('*.less', {
+// //   // fis-parser-less 插件进行解析
+// //   parser: fis.plugin('less'),
+// //   // .less 后缀构建后改为 .css 文件
+// //   rExt: 'css'
+// // })
+
+
+// fis.match('::package', {
+//   // 静态资源前端加载器，纯前端项目必备插件。自动加载页面中用到的资源，同时还能按页面级别的All In One 打包
+//   postpackager: fis.plugin('loader')
+// });
+
+// fis.match('*.less', {
+//   parser: fis.plugin('less'),
+//   rExt: '.css'
+// });
+
+// fis.match('*.{less,css}', {
+//   packTo: '/static/aio.css'
+// })
+
+// fis.match('*.js', {
+//   packTo: '/static/aio.js'
+// })
+
+fis.match('::package', {
   postpackager: fis.plugin('loader', {
-    //allInOne: true
+    allInOne: true
   })
-});
+})
 
-fis.match('*.{css,less}', {
-  packTo: '/static/aio.css'
-});
+fis.match('*.less', {
+  parser: fis.plugin('less'),
+  rExt: '.css'
+})
